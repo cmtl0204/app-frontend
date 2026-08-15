bf1f07f9ad278ccfb26277ded93d74f12c0a6304
cmtl <cesar.tamayo0204@gmail.com>
2026-08-15 09:05:56 -0500

    fix bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
-    APP_URL: 'https://siturin.produccion.gob.ec',
-    APP_SHORT_NAME: 'SITURIN',
-    APP_NAME: 'Sistema de Turismo Inteligente',
+    APP_URL: 'https://app-frontend.amauta.education',
+    APP_SHORT_NAME: 'APP',
+    APP_NAME: 'Sistema APP',
     APP_PATH_ASSETS: 'production',
     APP_VERSION: '3.0.0',
-    API_URL: 'https://api-siturin.produccion.gob.ec/api/v1',
-    API_ASSETS: 'https://api-siturin.produccion.gob.ec/assets',
+    API_URL: 'https://api-app-frontend.amauta.education/api/v1',
+    API_ASSETS: 'https://api-app-frontend.amauta.education/assets',
 };
