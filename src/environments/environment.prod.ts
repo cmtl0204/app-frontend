079bfcb7065d7da45ca90c6461ea18a19c36f374
cmtl <cesar.tamayo0204@gmail.com>
2026-08-15 09:23:45 -0500

    fix bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
-    APP_URL: 'https://app-frontend.amauta.education',
+    APP_URL: 'https://app.amauta.education',
     APP_SHORT_NAME: 'APP',
     APP_NAME: 'Sistema APP',
     APP_PATH_ASSETS: 'production',
     APP_VERSION: '3.0.0',
-    API_URL: 'https://api-app-frontend.amauta.education/api/v1',
-    API_ASSETS: 'https://api-app-frontend.amauta.education/assets',
+    API_URL: 'https://api-app.amauta.education/api/v1',
+    API_ASSETS: 'https://api-app.amauta.education/assets',
 };
